dd678dd1874f3eda14c049c0ab0ee85ae12cb656
Corey Rolton <c_r137@student.usc.edu.au>
2025-05-30 16:29:41 +1000

    Maps comments added

--- src/main/java/dungeon/engine/Maps.java
+++ src/main/java/dungeon/engine/Maps.java
@@ -4,6 +4,10 @@ public class Maps {
 
     private final int[][] mapValues;
 
+    /**
+     * Initializes the map walls into a 2d array.
+     *
+     */
     public Maps() {
         mapValues = new int[][] {
                 {0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
@@ -20,6 +24,11 @@ public class Maps {
         };
     }
 
+    /**
+     * The walls of the map.
+     *
+     * @return the walls, which is a 2d array.
+     */
     public Object[] getMapVaules() {
         return this.mapValues;
     }
